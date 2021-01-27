db5447e2c9c5a2ccab3bc3f670a3804b358742d1
ilyaKodit <ilyaKodit@mail.ru>
2021-01-27 16:42:31 +0300

    env and config

---
+++ frontend/config.ts
@@ -0,0 +1,14 @@
+const NODE_ENV = process.env.NODE_ENV;
+
+const MONGO_CLUSTER = process.env.MONGO_CLUSTER;
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PASS = process.env.MONGO_PASS;
+const MONGO_DB = process.env.MONGO_DB;
+
+export {
+  NODE_ENV, 
+  MONGO_CLUSTER, 
+  MONGO_USER, 
+  MONGO_PASS, 
+  MONGO_DB
+}
